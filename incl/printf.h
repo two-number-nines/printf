df11eb5573c04b1f5c4f5d9fc5942a1e146d74d1
Vincent Mulder <vmulder@f0r4s26.codam.nl>
2019-04-24 11:49:27 +0200

    changeOfPlan

--- incl/printf.h
+++ incl/printf.h
@@ -6,7 +6,7 @@
 /*   By: vmulder <vmulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/22 15:33:29 by vmulder        #+#    #+#                */
-/*   Updated: 2019/04/22 16:53:08 by vmulder       ########   odam.nl         */
+/*   Updated: 2019/04/24 11:45:54 by vmulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,16 @@
 # include <stdarg.h>
 # include <stdio.h> ///////delete this
 # include "../libft/libft.h"
+
+typedef struct	s_struct {
+	int			i;
+	const char	*format;
+	va_list		listpointer;
+	int			flags;
+	int			width;
+	int			precision;
+	int			length;
+	char		buf[1000];
+}				t_struct;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,68 +6,59 @@
 /*   By: vmulder <vmulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/22 15:42:55 by vmulder        #+#    #+#                */
-/*   Updated: 2019/04/23 15:57:13 by vmulder       ########   odam.nl         */
+/*   Updated: 2019/04/24 11:49:09 by vmulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/printf.h"
 
-void	csphaneler(const char *format, char c)
+void	ft_checkflags(t_struct val)
 {
-	(void)format;
-	if (c == 'c')
-	{
-		printf("hoi");
-	}
-	else if (c == 's')
-	{
-		printf("hoi");
-	}
-	else if (c == 'p')
-	{
-		printf("hoi");
-	}
+	
 }
 
-void	arghandeler(const char *format, char c)
+void	arghandeler(t_struct val)
 {
-	(void)format;
-	if (c == 'c' || c == 's' || c == 'p')
-		csphaneler(format, c);
+	ft_checkflags(val);
 }
 
-void	ft_strloop(const char *format, va_list listpointer)
+void	ft_strloop(t_struct val)
 {
-	int i;
-	(void)listpointer;
-	char buf[1000] = "";
-
-	i = 0;
-	while (format && format[i])
+	while (val.format && val.format[i])
 	{
 		
  		if (format[i] == '%')
- 			arghandeler(format, format[i + 1]);
+ 			arghandeler(val);
 		else
-			buf[j] = format[i];
+			val.buf[val.i] = val.format[val.i];
 		i++;
-		j++;
 	}
+}
+
+void	ft_initializer(const char *format, va_list listpointer)
+{
+	t_struct val;
+	val.i = 0;
+	val.listpointer = listpointer;
+	val.format = format;
 }
 
 int		ft_printf(const char *format, ...)
 {
 	va_list listpointer;
 	va_start(listpointer, format);
-	ft_strloop(format, listpointer);
+	ft_initializer(format, listpointer);
 	va_end(listpointer);
 	return (1);
 }
-
+char		ft_test(void)
+{
+	return('A');
+}
 int		main(void)
 {
 	//ft_printf("my printf: c\n", ft_atoi("10"), ft_atoi("20"), ft_atoi("30"));
-	ft_printf("my printf: %c\n");
+	printf("my printf:%c\n", ft_test());
 	//printf("real printf: c\n");
 	return (0);
 }
